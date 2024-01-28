48ef560ac556d87a7f0df467cedaf47e3dfbdfe2
mewlist <mewlist@mewlist.com>
2024-01-28 15:37:55 +0900

    Add AutoContextLoader

---
+++ Runtime/Context/AutoContextLoader.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Mew.Core.Assets;
+using UnityEngine;
+
+namespace Doinject
+{
+    public class AutoContextLoader : MonoBehaviour, IInjectableComponent
+    {
+        [SerializeField] private List<GameObjectContext> gameObjectContextPrefabs;
+        [SerializeField] private List<UnifiedScene> sceneContexts;
+
+        private IContext Context { get; set; }
+
+        [Inject]
+        // ReSharper disable once UnusedMember.Global
+        public void Construct(IContext context)
+        {
+            Context = context;
+        }
+
+        public async Task OnInjected()
+        {
+            foreach (var gameObjectContextPrefab in gameObjectContextPrefabs)
+                await Context.GameObjectContextLoader.LoadAsync(gameObjectContextPrefab);
+
+            foreach (var sceneContext in sceneContexts)
+                await Context.SceneContextLoader.LoadAsync(sceneContext, active: true);
+        }
+    }
+}
