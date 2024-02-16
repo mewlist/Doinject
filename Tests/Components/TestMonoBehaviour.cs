636e5d04136d4093ac16eaa1f092dcd810cbd139
mewlist <mewlist@mewlist.com>
2024-02-17 02:08:45 +0900

    Add DynamicInjectable test

--- Tests/Components/TestMonoBehaviour.cs
+++ Tests/Components/TestMonoBehaviour.cs
@@ -5,11 +5,13 @@ namespace Doinject.Tests
     public class TestMonoBehaviour : MonoBehaviour, IInjectableComponent
     {
         public InjectedObject InjectedObject { get; private set; }
+        public int InjectedCount { get; private set; }
 
         [Inject]
         public void Inject(InjectedObject injectedObject)
         {
             InjectedObject = injectedObject;
+            InjectedCount++;
         }
     }
 }
